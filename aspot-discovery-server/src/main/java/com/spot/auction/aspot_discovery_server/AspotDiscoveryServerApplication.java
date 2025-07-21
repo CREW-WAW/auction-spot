bb5cf56a705a5d9ce414086d45ac32dc9e0e5169
kdkrkwhr <kdkdongki1997@gmail.com>
2025-07-21 15:23:01 +0900

    [feat] eureka 서버 설정 추가

--- aspot-discovery-server/src/main/java/com/spot/auction/aspot_discovery_server/AspotDiscoveryServerApplication.java
+++ aspot-discovery-server/src/main/java/com/spot/auction/aspot_discovery_server/AspotDiscoveryServerApplication.java
@@ -2,8 +2,10 @@ package com.spot.auction.aspot_discovery_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class AspotDiscoveryServerApplication {
 
 	public static void main(String[] args) {
